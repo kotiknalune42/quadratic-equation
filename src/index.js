0ee0ec7c0d4b37b6b59f6f50c85de1cf487bb389
Катрин Райлян <k.shalamowa@gmail.com>
2018-09-21 00:01:37 +0300

    Quadratic equation: added sorting

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
-module.exports =  function solveEquation(equation){
-	var arr = (equation.split(' '));
-  
-  /* "-20", "*", "x^2", "-", "108797540", "*", "x", "-", "130011773690520" */
-  
- var a = Number(arr[0]);
- var b = Number(arr[4]);;
- var c = Number(arr[8]);;
+function solveEquation(equation){
+	var arr = (equation.split(' ')),
+	    a = Number(arr[0]),
+	    b = Number(arr[4]),
+	    c = Number(arr[8]);
  
- if(arr[3] == "-") {b = b*-1;};
- if(arr[7] == "-") {c = c*-1;};
+ if(arr[3] == "-") {b = b*-1;}
+ if(arr[7] == "-") {c = c*-1;}
  
-var x1 = -b/ 2 /a + Math.pow(Math.pow(b,2) - 4 * a * c , 0.5) / 2 / a;
-var x2 = -b/ 2 / a -Math.pow(Math.pow(b,2) - 4 * a * c ,0.5) / 2 / a;
+ var x1 = -b/ 2 /a + Math.pow(Math.pow(b,2) - 4 * a * c , 0.5) / 2 / a;
+ var x2 = -b/ 2 / a -Math.pow(Math.pow(b,2) - 4 * a * c ,0.5) / 2 / a;
  
- var answer1 = Math.round(x1);
- var answer2 = Math.round(x2);
+ var answer1 = Math.round(x1),
+     answer2 = Math.round(x2);
  
- return ([answer1, answer2]);
+ if(answer1 < answer2){return ([answer1,answer2]);}
+ else {return ([answer2,answer1]);}
 }
