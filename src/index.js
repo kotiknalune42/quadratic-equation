44543feec7046e6d28cd271a492fb49ff0336dbe
Катрин Райлян <k.shalamowa@gmail.com>
2018-09-21 00:03:35 +0300

    Quadratic equation: fixed error

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 function solveEquation(equation){
-	var arr = (equation.split(' ')),
-	    a = Number(arr[0]),
-	    b = Number(arr[4]),
-	    c = Number(arr[8]);
+	var arr = (equation.split(' '));
+  
+  /* "-20", "*", "x^2", "-", "108797540", "*", "x", "-", "130011773690520" */
+  
+ var a = Number(arr[0]);
+ var b = Number(arr[4]);
+ var c = Number(arr[8]);
  
  if(arr[3] == "-") {b = b*-1;}
  if(arr[7] == "-") {c = c*-1;}
  
- var x1 = -b/ 2 /a + Math.pow(Math.pow(b,2) - 4 * a * c , 0.5) / 2 / a;
- var x2 = -b/ 2 / a -Math.pow(Math.pow(b,2) - 4 * a * c ,0.5) / 2 / a;
+var x1 = -b/ 2 /a + Math.pow(Math.pow(b,2) - 4 * a * c , 0.5) / 2 / a;
+var x2 = -b/ 2 / a -Math.pow(Math.pow(b,2) - 4 * a * c ,0.5) / 2 / a;
  
- var answer1 = Math.round(x1),
-     answer2 = Math.round(x2);
+ var answer1 = Math.round(x1);
+ var answer2 = Math.round(x2);
  
- if(answer1 < answer2){return ([answer1,answer2]);}
- else {return ([answer2,answer1]);}
+ if(answer1 < answer2){ 			       
+ 		console.log([answer1,answer2]);}
+ else {
+ 		console.log([answer2,answer1]);}
 }
+
+solveEquation('294 * x^2 - 141195558 * x - 1600964090384736')
