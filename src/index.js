6fd02964e42ab0e298eac441d0c268d9ca42da2f
Катрин Райлян <k.shalamowa@gmail.com>
2018-09-20 23:31:22 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function solveEquation(equation) {
-  // your implementation
+module.exports =  function solveEquation(equation){
+	var arr = (equation.split(' '));
+  
+  /* "-20", "*", "x^2", "-", "108797540", "*", "x", "-", "130011773690520" */
+  
+ var a = Number(arr[0]);
+ var b = Number(arr[4]);;
+ var c = Number(arr[8]);;
+ 
+ if(arr[3] == "-") {b = b*-1;};
+ if(arr[7] == "-") {c = c*-1;};
+ 
+var x1 = -b/ 2 /a + Math.pow(Math.pow(b,2) - 4 * a * c , 0.5) / 2 / a;
+var x2 = -b/ 2 / a -Math.pow(Math.pow(b,2) - 4 * a * c ,0.5) / 2 / a;
+ 
+ var answer1 = Math.round(x1);
+ var answer2 = Math.round(x2);
+ 
+ return ([answer1, answer2]);
 }
